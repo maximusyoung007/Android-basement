86e485f9c591d621ebb0a0dc60850e765c7f43df
maximusyoung007 <1091261998@qq.com>
2018-04-06 11:49:04 +0800

    activitytest

--- ActivityTest/app/src/main/java/com/example/activitytest/FirstActivity.java
+++ ActivityTest/app/src/main/java/com/example/activitytest/FirstActivity.java
@@ -1,6 +1,7 @@
 package com.example.activitytest;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -8,12 +9,26 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+import android.util.Log;
 
 public class FirstActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main,menu);
         return true;
+    }
+    @Override
+    protected void onActivityResult(int requestCode,int resultCode,Intent data)
+    {
+        switch(requestCode){
+            case 1:
+                if(resultCode == RESULT_OK){
+                    String returnedData = data.getStringExtra("data_return");
+                    Log.d("FirstActivity",returnedData);
+                }
+                break;
+                default:
+        }
     }
     @Override
     public boolean onOptionsItemSelected(MenuItem item){
@@ -34,11 +49,44 @@ public class FirstActivity extends AppCompatActivity {
         Button button1 = (Button) findViewById(R.id.button_1);
         button1.setOnClickListener(new View.OnClickListener(){//设置一个监听器
             @Override
+            //在活动销毁时返回一个结果给上一个活动
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(FirstActivity.this,SecondActivity.class);
+                startActivityForResult(intent,1);
+            }
+            /*@Override
+            //pass a string from Firstactivity to Secondactivity
+            public void onClick(View v)
+            {
+                String data = "Hello SecondActivity";
+                Intent intent = new Intent(FirstActivity.this,SecondActivity.class);
+                intent.putExtra("extra_data",data);
+                //we can find in logcat that we successfully pass the String to secondanctivity
+                startActivity(intent);
+            }*/
+           /* @Override
+           //调用系统拨号界面
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:10086"));
+                startActivity(intent);
+            }*/
+           /* @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                //setData(),acept a uri item
+                intent.setData(Uri.parse("http://www.baidu.com"));
+                startActivity(intent);
+            }*/
+            /*@Override
             public void onClick(View v){
                 Intent intent = new Intent("com.example.activitytest.ACTION_START");
                 intent.addCategory("com.example.activitytest.MY_CATEGORY");
                 startActivity(intent);
-            }
+            }*/
             /*explicit intent
             @Override
             public void onClick(View v){
--- ActivityTest/app/src/main/java/com/example/activitytest/SecondActivity.java
+++ ActivityTest/app/src/main/java/com/example/activitytest/SecondActivity.java
@@ -2,12 +2,36 @@ package com.example.activitytest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity {
-
+    @Override
+    public void onBackPressed(){
+        Intent intent = new Intent();
+        intent.putExtra("data-return","Hello FirstActivity");
+        setResult(RESULT_OK,intent);//向上一个活动返回处理结果
+        finish();
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.secondlayout);
+        //get data passed by Firstactivity
+       /* Intent intent = getIntent();
+        String data = intent.getStringExtra("extra_data");
+        Log.d("SecondActivity",data);*/
+       Button button2 = (Button) findViewById(R.id.button_2);
+       button2.setOnClickListener(new View.OnClickListener() {//给按钮2注册点击事件
+           @Override
+           public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.putExtra("data_return","Hello FirstActivity");
+                setResult(RESULT_OK,intent);//向上一个活动返回处理结果
+                finish();
+           }
+       });
     }
 }
