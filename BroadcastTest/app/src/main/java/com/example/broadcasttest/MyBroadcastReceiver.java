893c7aeaaf4eaeb949c64e354c25a75792ae5c21
maximusyoung007 <1091261998@qq.com>
2018-04-13 10:45:06 +0800

    Broadcasttest

---
+++ BroadcastTest/app/src/main/java/com/example/broadcasttest/MyBroadcastReceiver.java
@@ -0,0 +1,18 @@
+package com.example.broadcasttest;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+/**
+ * Created by Administrator on 2018/4/13.
+ */
+
+public class MyBroadcastReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context,Intent intent){
+        Toast.makeText(context,"received in MyBroadcastReceiver",Toast.LENGTH_SHORT).show();
+        abortBroadcast();
+    }
+}
